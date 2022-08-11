c5be5b3b35b40685f07368482addf6ede7248a56
Daniel-J-Mason <daniel.j.msn@gmail.com>
2022-08-11 10:57:25 -0400

    Update Calculator Model and Create method to calculate string patterns directly

--- src/main/java/com/dev/calculatorapp/Calculator.java
+++ src/main/java/com/dev/calculatorapp/Calculator.java
@@ -1,35 +1,79 @@
 package com.dev.calculatorapp;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
-    private float currentTotal;
-    private float previousTotal;
+    private float total;
 
     public Calculator(){
-        currentTotal = 0;
-        previousTotal = 0;
+        total = 0;
+    }
+    
+    public void calculateStringPattern(String input){
+        Pattern binaryOperation = Pattern.compile("(\\d+.?\\d*) ?([+\\-/x]) ?(\\d+.?\\d*)");
+        Pattern unaryOperation = Pattern.compile("([+\\-/x]) ?(\\d+.?\\d*)");
+        Pattern simpleNumber = Pattern.compile("\\d+.?\\d*");
+        
+        Matcher binaryMatch = binaryOperation.matcher(input);
+        Matcher unaryMatch = unaryOperation.matcher(input);
+        Matcher simpleMatch = simpleNumber.matcher(input);
+        
+        if (binaryMatch.matches()) {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Binary Group " + i + " " + binaryMatch.group(i));
+            }
+        } else if (unaryMatch.matches()) {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Unary Group " + i + " " + unaryMatch.group(i));
+            }
+        } else if (simpleMatch.matches()) {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Number Group " + i + " " + simpleMatch.group(i));
+            }
+        }
+    }
+    
+    public void calculate(float x, float y, String operator){
+        switch (operator) {
+            case "+":
+                total = x + y;
+                break;
+            case "-":
+                total = x - y;
+                break;
+            case "/":
+                total = x / y;
+                break;
+            case "x":
+                total = x * y;
+                break;
+        }
+    }
+    
+    public void calculate(float x, String operator){
+        switch (operator) {
+            case "+":
+                total += x;
+                break;
+            case "-":
+                total -= x;
+                break;
+            case "/":
+                total /= x;
+                break;
+            case "x":
+                total *= x;
+                break;
+        }
     }
 
-    private void update(){
-        previousTotal = currentTotal;
+    public float getTotal() {
+        return total;
     }
-
-    public float add(float x){
-        currentTotal = previousTotal + x;
-        update();
-        return currentTotal;
+    
+    public void setTotal(float total) {
+        this.total = total;
     }
-
-    public float subtract(float x) {
-        currentTotal = previousTotal - x;
-        update();
-        return currentTotal;
-    }
-
-    public float getCurrentTotal() {
-        return currentTotal;
-    }
-
-    public float getPreviousTotal() {
-        return previousTotal;
-    }
+    
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,10 +1,46 @@
 import com.dev.calculatorapp.Calculator;
-import org.junit.jupiter.api.*;
-public class CalculatorTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+public class CalculatorTest {
+    
+    Calculator calculator = null;
     @BeforeEach
     public void initializeCalculator(){
-        Calculator calculator = new Calculator();
-
+        calculator = new Calculator();
+    }
+    
+    @Test
+    public void addition(){
+        calculator.calculate(20, 20, "+");
+        Assertions.assertEquals(40, calculator.getTotal());
+        calculator.calculate(20, "+");
+        Assertions.assertEquals(60, calculator.getTotal());
+        calculator.calculate(-60 ,"+");
+        Assertions.assertEquals(0, calculator.getTotal());
+    }
+    
+    @Test
+    public void subtraction(){
+        calculator.calculate(20, 20, "-");
+        Assertions.assertEquals(0, calculator.getTotal());
+        calculator.calculate(27, "-");
+        Assertions.assertEquals(-27, calculator.getTotal());
+        calculator.calculate(-27, "-");
+        Assertions.assertEquals(0, calculator.getTotal());
+    }
+    
+    @Test
+    public void multiplication(){
+    }
+    
+    @Test
+    public void division(){
+    
+    }
+    
+    @Test
+    public void printouts(){
     }
 }
